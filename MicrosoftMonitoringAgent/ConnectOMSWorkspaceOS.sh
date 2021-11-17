7222e1db8f7c9b98cc841d16103cdcf5ba286d63
wernerrall147 <wernerrall@live.com>
2021-11-17 15:15:28 +0200

    OMSLINux commands added

--- MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
+++ MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
@@ -1,2 +1,6 @@
 sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w <workspace> -s <keys> -v
 sudo /opt/microsoft/omsagent/bin/service_control restart
+
+##Use this for purging
+## wget https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/tools/purge_omsagent.sh
+## sudo sh purge_omsagent.sh
