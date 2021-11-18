8a35ab42058be6b7390d992609a569a97be5325d
wernerrall147 <wernerrall@live.com>
2021-11-18 17:13:50 +0200

    Updated cmdlets 36

--- MicrosoftMonitoringAgent/ConnectExtensionListOfMachines.ps1
+++ MicrosoftMonitoringAgent/ConnectExtensionListOfMachines.ps1
@@ -10,7 +10,7 @@ Connect-AzAccount -Tenant $TenantID | Out-Null
 #Login-AzAccount
 $WsSubID = Read-Host "Enter the subscription where your Log Analytics Workspace is"
 $WsLAID = Read-Host "Enter your Log Analytics Workspace Name"
-Select-AzSubscription -SubscriptionId "$WsSubID"
+Set-AzContext -SubscriptionId "$WsSubID"
 $workspaceName = "$WsLAID"
 
 #Get Workspace ID and Keys
@@ -27,13 +27,13 @@ $workspaceKey = (Get-AzOperationalInsightsWorkspaceSharedKeys -ResourceGroupName
 foreach ($v in $csv)
 {
     If ($v.OSType -like "*Windows*"){
-Select-AzSubscription -SubscriptionId "$v.SUBSCRIPTIONID"
+        Set-AzContext -SubscriptionId "$v.SUBSCRIPTIONID"
 Set-AzVMExtension -ResourceGroupName $v.RSG -VMName $v.OSNAME -Name 'MicrosoftMonitoringAgent' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'MicrosoftMonitoringAgent' -TypeHandlerVersion '1.0' -Location $v.Loc -SettingString "{'workspaceId':  '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey' }"
 Write-Host "$v.OSNAME Windows VM has been updated"
 }
 else
 {
-Select-AzSubscription -SubscriptionId "$v.SUBSCRIPTIONID"
+    Set-AzContext -SubscriptionId "$v.SUBSCRIPTIONID"
 Set-AzVMExtension -ResourceGroupName $v.RSG -VMName $v.OSNAME -Name 'OmsAgentForLinux' -Publisher 'Microsoft.EnterpriseCloud.Monitoring' -ExtensionType 'OmsAgentForLinux' -TypeHandlerVersion '1.0' -Location $v.Loc -SettingString "{'workspaceId':  '$workspaceId'}" -ProtectedSettingString "{'workspaceKey': '$workspaceKey' }"
 Write-Host "$v.OSNAME Linux VM has been updated"
 }
