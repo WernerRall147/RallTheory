c5efec77013e39fa043acbfc856488dbb4f18717
wernerrall147 <wernerrall@live.com>
2021-11-25 12:43:09 +0200

    added linux cmds

--- MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
+++ MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
@@ -4,3 +4,16 @@ sudo /opt/microsoft/omsagent/bin/service_control restart
 ##Use this for purging
 ## wget https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/tools/purge_omsagent.sh
 ## sudo sh purge_omsagent.sh
+
+###Use this for install inside the OS
+## 
+## 
+
+###Use this for adding the extension
+## az vm extension set \
+##  --resource-group myResourceGroup \
+##  --vm-name myVM \
+##  --name OmsAgentForLinux \
+##  --publisher Microsoft.EnterpriseCloud.Monitoring \
+##  --protected-settings '{"workspaceKey":"myWorkspaceKey"}' \
+##  --settings '{"workspaceId":"myWorkspaceId"}'
