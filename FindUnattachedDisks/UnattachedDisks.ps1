9c6bd2af55e3323115bd70a942cc30d5e0e9664e
wernerrall147 <wernerrall@live.com>
2021-12-06 12:36:28 +0200

    added disks script

---
+++ FindUnattachedDisks/UnattachedDisks.ps1
@@ -0,0 +1,17 @@
+# Set deleteUnattachedDisks=1 if you want to delete unattached Managed Disks
+# Set deleteUnattachedDisks=0 if you want to see the Id of the unattached Managed Disks
+$deleteUnattachedDisks=0
+$managedDisks = Get-AzDisk
+foreach ($md in $managedDisks) {
+    # ManagedBy property stores the Id of the VM to which Managed Disk is attached to
+    # If ManagedBy property is $null then it means that the Managed Disk is not attached to a VM
+    if($md.ManagedBy -eq $null){
+        if($deleteUnattachedDisks -eq 1){
+            Write-Host "Deleting unattached Managed Disk with Id: $($md.Id)"
+            $md | Remove-AzDisk -Force
+            Write-Host "Deleted unattached Managed Disk with Id: $($md.Id) "
+        }else{
+            $md.Id
+        }
+    }
+ }
