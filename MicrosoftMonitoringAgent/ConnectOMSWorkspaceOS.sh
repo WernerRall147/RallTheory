d48642d29160a280397fa9b447e72df684b7b940
wernerrall147 <wernerrall@live.com>
2021-12-06 13:04:44 +0200

    added disk graph query

--- MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
+++ MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
@@ -6,8 +6,7 @@ sudo /opt/microsoft/omsagent/bin/service_control restart
 ## sudo sh purge_omsagent.sh
 
 ###Use this for install inside the OS
-## 
-## 
+## wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w <YOUR WORKSPACE ID> -s <YOUR WORKSPACE PRIMARY KEY>
 
 ###Use this for adding the extension
 ## az vm extension set \
