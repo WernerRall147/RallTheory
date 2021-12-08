84bf0ff4af9ed0029c64c0f12d104112be2b1ed9
wernerrall147 <wernerrall@live.com>
2021-12-08 10:53:04 +0200

    working on linux sh

--- MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
+++ MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
@@ -1,5 +1,32 @@
-sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w <workspace> -s <keys> -v
-sudo /opt/microsoft/omsagent/bin/service_control restart
+sudo su omsagent
+WS_ASGN="^([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})$"
+var="/opt/microsoft/omsagent/bin/omsadmin.sh -l"
+$var | grep -P -q $WS_ASGN
+echo $?
+
+DIR="/opt/microsoft/omsconfig/"
+CURWS=$(sudo su omsagent -c '/opt/microsoft/omsagent/bin/omsadmin.sh -l')
+WSID="<enter ID>"
+SK="<enter SK>"
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ### if RedHat change line 7 to Python3 ###
+  echo "DIR Found, checking the install"
+  sudo su omsagent -c 'python /opt/microsoft/omsconfig/Scripts/PerformRequiredConfigurationChecks.py'
+  echo "Adding Correct Workspace"
+  sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w $WSID -s $SK
+  sudo /opt/microsoft/omsagent/bin/service_control restart
+else
+  echo "${DIR} not found. Starting the install."
+  wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WSID -s $SK
+  sudo /opt/microsoft/omsagent/bin/service_control restart
+  exit 0
+fi
+
+
+##sudo su omsagent -c '/opt/microsoft/omsagent/bin/omsadmin.sh -l'
+##wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w <YOUR WORKSPACE ID> -s <YOUR WORKSPACE PRIMARY KEY>
+##sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w <workspace> -s <keys> -v
+##sudo /opt/microsoft/omsagent/bin/service_control restart
 
 ##Use this for purging
 ## wget https://raw.githubusercontent.com/microsoft/OMS-Agent-for-Linux/master/tools/purge_omsagent.sh
