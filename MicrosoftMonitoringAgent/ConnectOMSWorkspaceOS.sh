aa9eb9040220ebc84cc2240267c4e7c2921e7d11
wernerrall147 <wernerrall@live.com>
2021-12-09 16:23:47 +0200

    Script working, added comments

--- MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
+++ MicrosoftMonitoringAgent/ConnectOMSWorkspaceOS.sh
@@ -1,29 +1,34 @@
-sudo su omsagent
-if [ "$result" = "$WSID" ]; then 
-echo "Strings are equal" 
-else 
-echo "nope" 
-fi
+## Use the below when running interactive
+#read -p 'Log Analytics Workspace ID: ' WSID
+#read -p 'Log Analytics Workspace Shared Key: ' SK
 
-
+#Update the below details with your Log Analytics Workspace
+WSID="<Ws key>"
+SK="<sk key>"
+#Directory to check if OMS agent has ever been installed
 DIR="/opt/microsoft/omsconfig/"
-WSID="69860496-a11d-4692-8c79-fa89789c406e"
-SK="<enter SK>"
 if [ -d "$DIR" ]; then
-  ### Take action if $DIR exists ### if RedHat change line 7 to Python3 ###
-  echo "DIR Found, checking the install"
+  ### Take action if $DIR exists
+  echo "Agent Directory Found, checking the install"
+  #regex to look for Workspace ID 
   WS_ASGN="([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})"
+  #CD to OMSAgent Dir
   cd /etc/opt/microsoft/omsagent
+  #Find Workspace ID in dir
   var=dir
+  #Extract WorkspaceID
   result=$($var | grep -E -o $WS_ASGN)
+  #If Workspace is the same as required Workspace then only restart the service
   if [ "$result" = "$WSID" ]; then 
   echo "Correct Workspace is added already"
   sudo /opt/microsoft/omsagent/bin/service_control restart
+  #If the workspace is not the same add the correct workspace
   else 
   echo "Adding Correct Workspace"
   sudo /opt/microsoft/omsagent/bin/omsadmin.sh -w $WSID -s $SK
   sudo /opt/microsoft/omsagent/bin/service_control restart
   fi 
+#If directory is not found then install the OMS Agent for Linux
 else
   echo "${DIR} not found. Starting the install."
   wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WSID -s $SK
