c2fc0241fc6096d93b6d323650b2dbc2f7f487c1
wernerrall147 <wernerrall@live.com>
2022-04-11 12:01:22 +0200

    Optimized

--- DNSPrivateZones/DNSPrivateZones.ps1
+++ DNSPrivateZones/DNSPrivateZones.ps1
@@ -1,13 +1,18 @@
-Connect-AzAccount
+#Require-Module -Name Az.PrivateDns
+$TenantID = Read-host -Prompt "Please enter your Tenant ID"
+$SubID = Read-host -Prompt "Please enter your subscription ID"
+$RSG = Read-host -Prompt "Please enter your Recource Group Name that contains your virtual network"
+$VnetName = Read-host -Prompt "Please enter your Virtual Network Name"
+$SubNet = Read-host -Prompt "Please enter your Virtual Network Subnet Name"
 
-Install-Module -Name Az.PrivateDns -force
+Connect-AzAccount -Tenant $TenantID -SubscriptionID $subID
 
-$backendSubnet = get-AzVirtualNetworkSubnetConfig -Name PVTE `
-$vnet = get-AzVirtualNetwork -ResourceGroupName vTec-IaaS-Hub -Location UKSouth `
--Name cakepokeruk -AddressPrefix 10.0.2.0/16 -Subnet $backendSubnet `
+$csv = import-csv -Path .\PrivateDNSZones.csv
 
-New-AzPrivateDnsZone -Name privatelink.azure-automation.cn -ResourceGroupName vTec-IaaS-Hub
+$vnet = get-AzVirtualNetwork -ResourceGroupName $RSG -Name $VnetName
 
-New-AzPrivateDnsVirtualNetworkLink -ZoneName privatelink.azure-automation.cn `
--ResourceGroupName vTec-IaaS-Hub `
--Name -VirtualNetworkId 3fd83162-8ced-40c6-bd0d-de790ac03a06
+
+foreach($z in $csv){
+New-AzPrivateDnsZone -Name $z.PrivateDNSzonename -ResourceGroupName $vnet.ResourceGroupName
+New-AzPrivateDnsVirtualNetworkLink -ZoneName $z.PrivateDNSzonename -ResourceGroupName $vnet.ResourceGroupName -Name "HubLink" -VirtualNetworkId $vnet.Id
+}
