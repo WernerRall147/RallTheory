395bef6f87571ad4f87c82d093f649d8f33ac6f6
Wernerrall147 <wernerrall@live.com>
2022-07-22 14:51:43 +0200

    added sqlrightsizing

---
+++ SQLManagedInstanceRightSizing/RunAssessment.ps1
@@ -0,0 +1,5 @@
+#This will collect performance data
+.\SqlAssessment.exe --configfile C:\Temp\SQL\config.json
+
+#outputFolder is scanned for files created by the PerfDataCollection action, and recommendations are provided for every instance found
+.\SqlAssessment.exe --configFile C:\Temp\SQL\skurecconfig.json
