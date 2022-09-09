52cc6d8019102072f2a6bd8ce1fb26ecc7e1a794
Wernerrall147 <wernerrall@live.com>
2022-09-09 12:29:08 +0200

    Updated the configuration to include the DNS Configuration Add

--- PrivateEndpointDNSCreateandConfig/PrivateEndpointDNSCreateandConfig.ps1
+++ PrivateEndpointDNSCreateandConfig/PrivateEndpointDNSCreateandConfig.ps1
@@ -1,53 +1,10 @@
-<##############Original Script
-
-Set-AzContext -Subscription "sA"
-New-AzDiskAccess -ResourceGroupName "rA" -Name "" -Location "lA"
-
-###################Original Script2
-
-Connect-AzAccount
-
-Set-AzContext -Subscription "sA"
-$daccname = ""
-$resourceid = Get-AzResource -ResourceName "" -ResourceGroupName "rA" #TODO
-$resourceid.ResourceId
-
-Set-AzContext -Subscription "sB" #TODO
-
-$rsgname = "rB"
-$rsname = ""
-$subname = ""
-$privatelinkid = $resourceid.ResourceId
-$pename = $daccname
-
-$virtualNetwork = Get-AzVirtualNetwork -ResourceName $rsname -ResourceGroupName $rsgname#TODO
-$subnet = $virtualNetwork | Select-Object -ExpandProperty subnet | Where-Object Name -eq ""
-$plsconnection = New-AzPrivateLinkServiceConnection -Name $pename -privatelinkserviceid $privatelinkid -groupid disks #TODO
-New-AzPrivateEndpoint -Name $pename -ResourceGroupName "rC" -Location "" -PrivateLinkServiceConnection $plsconnection -Subnet $subnet
-
-$vnet = Get-AzPrivateEndpoint -ResourceGroupName "rC" -Name $daccname
-$name2 = $vnet.CustomDnsConfigs.fqdn #TODO
-$ipadress = $vnet.CustomDnsConfigs.ipadresses
-$name3 = $name2.Split(".") #TODO
-$alias = $name3[0]+"."+$name3[1]
-$cname = $name3[0]+".privatelink.blob.core.windows.net"
-
-$privatelinkoutput = $name3[0]+".privatelink.blob.core.windows.net"+$ipadress
-Write-Output "........."
-
-Set-AzContext -Subscription "sB" #TODO
-New-AzPrivateDnsRecordSet -Name $name3[0] -RecordType A -ResourceGroupName "rB" -Ttl 3600 -ZoneName ".privatelink.blob.core.windows.net" -PrivateDnsRecord ()
-New-AzPrivateDnsRecordSet -Name $alias -RecordType CNAME -ResourceGroupName "rB" -Ttl 3600 -ZoneName "blob.storage.azure.net" -PrivateDnsRecord (New-AzPrivateDnsRecordConfig)
-
-########################################################################################################
-
 ##Case
 # We want to create Private Endpoint and add to Private DNS Zones for Disk Access
 #
 ## What input do I need
 ## What steps do I need to take
 ## What is my outcome
-#>
+#
 [CmdletBinding()]
 param (
     #[Parameter()]$AppID = "",
@@ -70,7 +27,9 @@ param (
     #[Parameter()]$customDNSName,
     #[Parameter()]$dnsAlias,
     #[Parameter()]$dnsCName,
-    #[Parameter()]$ipadress
+    #[Parameter()]$ipadress,
+    #[Parameter()]$dnsZoneObject,
+    #[Parameter()]$dnszoneConfig
 )
 function azureAuth {
     #request AppID and Secret and create a secure Object
@@ -102,17 +61,18 @@ function newPrivateEndpoint {
     
     $customDNSName = ($pename.CustomDnsConfigs.fqdn).Split(".blob.storage.azure.net")
     $ipadress = $pename.CustomDnsConfigs.ipaddresses
-    #$name3 = $name2.Split(".")
-    #$dnsAlias = $customDNSName
     $dnsCName = $customDNSName[0]+".privatelink.blob.core.windows.net"
 
     New-AzPrivateDnsRecordSet -Name $customDNSName[0] -RecordType A -ResourceGroupName $vnetResourceGroup -Ttl 3600 -ZoneName "privatelink.blob.core.windows.net" -PrivateDnsRecords (New-AzPrivateDnsRecordConfig -Ipv4Address $ipadress)
     New-AzPrivateDnsRecordSet -Name $customDNSName[0] -RecordType CNAME -ResourceGroupName $vnetResourceGroup -Ttl 3600 -ZoneName "blob.storage.azure.net" -PrivateDnsRecord (New-AzPrivateDnsRecordConfig -Cname $dnsCName)
-    
+
+    $dnsZoneObject = Get-AzPrivateDnsZone -ResourceGroupName $vnetResourceGroup -Name 'privatelink.blob.core.windows.net'
+    $dnszoneConfig = New-AzPrivateDnsZoneConfig -Name 'privatelink.blob.core.windows.net' -PrivateDnsZoneId $dnsZoneObject.ResourceId
+    New-AzPrivateDnsZoneGroup -Name 'default' -ResourceGroupName $vnetResourceGroup -PrivateEndpointName $diskAccessObject.Name -PrivateDnsZoneConfig $dnszoneConfig 
+
 }
 
 azureAuth
 newDiskAccess
 newPrivateEndpoint
-
 
