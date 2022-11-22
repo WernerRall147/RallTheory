8c0bd1430e34c2bf2395fad8ffab1e5d9ca46b2d
Werner Rall <weral@microsoft.com>
2022-11-22 13:26:14 +0200

    Wrote a DSC for Windows Update that works well with Update Management

---
+++ WindowsUpdateDSC/WindowsUpdate.ps1
@@ -0,0 +1,46 @@
+Configuration WindowsUpdate {
+
+    Node localhost {
+
+        Registry WindowsUpdateAUWUServer {
+            Ensure = "Absent"
+            Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+            ValueName = "UseWUServer"
+            ValueData = "0"
+            ValueType = "Dword"
+        }
+
+        Registry WindowsUpdateAUWUStatusServer {
+            Ensure = "Absent"
+            Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+            ValueName = "WUStatusServer "
+            ValueData = "0"
+            ValueType = "Dword"
+        }
+
+        Registry WindowsUpdateAUOptions {
+            Ensure = "Present"
+            Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+            ValueName = "AUOptions"
+            ValueData = "4"
+            ValueType = "Dword"
+        }
+
+        Registry WindowsUpdateNoAU {
+            Ensure = "Present"
+            Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+            ValueName = "NoAutoUpdate"
+            ValueData = "0"
+            ValueType = "Dword"
+        }
+
+        Registry WindowsUpdateNoAutoRebootWithLoggedOnUsers {
+            Ensure = "Present"
+            Key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+            ValueName = "NoAutoRebootWithLoggedOnUsers"
+            ValueData = "1"
+            ValueType = "Dword"
+        }
+
+    }
+}
