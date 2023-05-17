f2be461a320d29d75671e2db678d6280e827af65
Werner Rall <weral@microsoft.com>
2023-05-17 17:48:02 +0200

    Updated Diagnostics settings and Diagnostics ps1 file

--- AzureSiteRecoveryDRRunbooks/DRDiagnostics.ps1
+++ AzureSiteRecoveryDRRunbooks/DRDiagnostics.ps1
@@ -55,10 +55,11 @@ $vmMap = $RecoveryPlanContext.VmMap
             $diagnosticsconfig_path = "$env:SystemDrive\temp\DiagnosticsConfiguration.json"
             Invoke-WebRequest -Uri $url -OutFile $diagnosticsconfig_path
 
-            $diagnosticsconfig_update1 = (Get-Content $diagnosticsconfig_path).Replace("(TODOUpdateResID)",$StorageAccountKey) | Set-Content $diagnosticsconfig_path 
+            $diagnosticsconfig_update1 = (Get-Content $diagnosticsconfig_path).Replace("(TODOUpdateResID)",$drres.Id) | Set-Content $diagnosticsconfig_path 
             $diagnosticsconfig_update2 = (Get-Content $diagnosticsconfig_path).Replace("(TODOUpdateStorac)",$DestinationStorageAccountName) | Set-Content $diagnosticsconfig_path 
+            $diagnosticsconfig_update1 = (Get-Content $diagnosticsconfig_path).Replace("(TODOUpdateStKey)",$StorageAccountKey) | Set-Content $diagnosticsconfig_path 
 
-            Set-AzVMDiagnosticsExtension -ResourceGroupName $VM.ResourceGroupName -VMName ($drres).Name -DiagnosticsConfigurationPath $diagnosticsconfig_path -StorageAccountName $DestinationStorageAccountName  -StorageAccountKey $StorageAccountKey
+            Set-AzVMDiagnosticsExtension -ResourceGroupName $VM.ResourceGroupName -VMName ($drres).Name -DiagnosticsConfigurationPath $diagnosticsconfig_path -storageAccountName $DestinationStorageAccountName -StorageAccountKey $StorageAccountKey
             }else {
                 Write-Output "Diagnostic Settings Correct"
             }
