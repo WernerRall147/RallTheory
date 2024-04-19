7ea589aacb38cd1089b3536c5a88597de3264726
Werner Rall <weral@microsoft.com>
2024-04-19 14:47:33 +0200

    Updated Azure Function

--- AzureFunctions/AzFunction_SpotVM_Evicted_StartUp.ps1
+++ AzureFunctions/AzFunction_SpotVM_Evicted_StartUp.ps1
@@ -1,7 +1,17 @@
-param($eventGridEvent, $TriggerMetadata)
+using namespace System.Net
 
-# Make sure to pass hashtables to Out-String so they're logged correctly
-$eventGridEvent | Out-String | Write-Host
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
 try {
     # Authenticate using managed identity or other secure methods (avoid interactive login)
     Connect-AzAccount -Identity 
@@ -27,3 +37,15 @@ catch {
     # Handle exceptions (log, return error response, etc.)
     Write-Error -Message $_.Exception
 }
+
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+
+if ($name) {
+    $body = "Hello, $name. This HTTP triggered function executed successfully."
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
